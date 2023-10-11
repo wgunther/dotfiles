766d3792c117a4ffb6d68474fb4a36f556c3c5cb
Will Gunther <will.gunther@duolingo.com>
2023-10-11 11:13:40 -0400

    Fix xl alias

--- aliases.sh
+++ aliases.sh
@@ -2,4 +2,4 @@ alias nvim=~/.local/nvim
 alias gs=~/.local/gs
 alias vim=nvim
 alias gcplogin="gcloud auth application-default login"
-alias xl="git log --oneline --graph --decorate $(git branch | tr -d ' *' | awk '{ print "master~1.."$0 }')"
+alias xl='git log --oneline --graph --decorate `git branch | tr -d " *" | awk "{ print \\"master~1..\\"\\$0 }"`'
