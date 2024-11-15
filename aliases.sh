99e1d1822dae39d06676217797ff04ff65e1cd88
Will Gunther <will.gunther@duolingo.com>
2024-11-15 10:07:22 -0500

    JJ config stuff

--- aliases.sh
+++ aliases.sh
@@ -19,12 +19,18 @@ alias clean_pyc="find . | grep -E '(/__pycache__$|\.pyc$|\.pyo$)' | xargs rm -rf
 git config --global alias.undo 'reset --soft HEAD^'
 # Do --update-refs by default on rebases.
 git config --global --add --bool rebase.updateRefs true
-
 git config --global alias.amend 'commit --amend  --no-edit'
 
 # nvim as default editor
 git config --global core.editor ~/.local/nvim
 gh config set editor ~/.local/nvim
+
+# jj aliases
+if command -v jj &> /dev/null; then
+  jj config set --user user.name $(git config user.name)
+  jj config set --user user.email $(git config user.email)
+  jj config set --user git.push-bookmark-prefix "wgunther-"
+fi
 
 export LS_COLORS='di=1;32:ln=1;30;47:so=30;45:pi=30;45:ex=1;31:bd=30;46:cd=30;46:su=30'
 export LS_COLORS="${LS_COLORS};41:sg=30;41:tw=30;41:ow=30;41:*.rpm=1;31:*.deb=1;31"
@@ -32,3 +38,8 @@ export LSCOLORS=CxahafafBxagagabababab
 zstyle ':completion:*' list-colors "${(@s.:.)LS_COLORS}"
 autoload -Uz compinit
 compinit
+
+# For jj autocomplete
+if command -v jj &> /dev/null; then
+    source <(jj util completion zsh)
+fi
--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ yes | sudo cpan Perl::Tidy
 
 # Copy files out to where they are supposed to be
 mkdir -p ~/.config/nvim
+mkdir -p ~/.config/jj
 ln -s $PWD/init.vim ~/.config/nvim/init.vim
 ln -s $PWD/tmux.conf ~/.tmux.conf
+ln -s $PWD/jj-config.toml ~/.config/jj/config.toml
 mkdir -p ~/.local
 ln -s $PWD/gs ~/.local/gs
 ln -s $PWD/xl ~/.local/xl
@@ -24,12 +26,23 @@ mv jj ~/.local/bin
 rm jj-*
 cd -
 
+# Set up jj environment
+pushd "/workspaces/${RepositoryName}"
+GIT_REMOTE=$(git remote)
+export GIT_MAIN=$(git remote show "${GIT_REMOTE}" | sed -n '/HEAD branch/s/.*: //p')
+envsubst '$GIT_MAIN' < "${JJ_BASE_CONFIG}" >> ~/.config/jj/config.toml
+jj git init --colocate
+# This branch tracking happens to work, but makes naming assumptions about refs.
+jj branch track $(git branch --format="%(upstream:lstrip=-1)@${GIT_REMOTE}")
+popd
+
 # Set up files
 touch ~/.bashrc
 touch ~/.zshrc
 # Backup
 cp ~/.zshrc ~/.zshrc.bak
 cp ~/.bashrc ~/.bashrc.bak
+
 # Add alias file
 VAR="source $PWD/aliases.sh"
 echo $VAR
